8fe8d4273262abaaa5ab1ff2fa299580d03b7bd9
= <shivampathak19972@gmail.com>
2026-02-16 22:24:10 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,99 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState, useEffect } from "react";
 
+const reviews = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag.",
+  },
+];
+
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const checkIndex = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return reviews.length - 1;
+    }
+    return number;
+  };
+
+  const nextSlide = () => {
+    setIndex((prevIndex) => checkIndex(prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setIndex((prevIndex) => checkIndex(prevIndex - 1));
+  };
+
+  useEffect(() => {
+    const slider = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(slider);
+  }, [index]);
+
+  const person = reviews[index];
+
+  return (
+    <div>
+      <h1 id="review-heading">Reviews</h1>
+
+      <div id="review-container">
+        <img
+          src={person.image}
+          alt={person.name}
+          id={`person-${index}-image`}
+          width="200"
+        />
+        <h3 id={`person-${index}`}>{person.name}</h3>
+        <h4>{person.title}</h4>
+        <p>{person.quote}</p>
+
+        <button className="prev" onClick={prevSlide}>
+          Previous
+        </button>
+        <button className="next" onClick={nextSlide}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
